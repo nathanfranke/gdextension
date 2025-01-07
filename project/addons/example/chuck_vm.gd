fca45841177aa64c7f11aff94f0344af11cd639f
Eric Max Kaplin <55173884+MaxKablaam@users.noreply.github.com>
2025-01-07 12:14:15 -0800

    Refactor ChuckVM to support multiple ChuckFiles and update test scene accordingly

--- project/addons/example/chuck_vm.gd
+++ project/addons/example/chuck_vm.gd
@@ -1,9 +1,9 @@
 class_name ChuckVM extends MyNode
 
-@export var chuck_file: ChuckFile
+@export var chuck_files: Array[ChuckFile]
 
 func _ready() -> void:
-	var chuck_path: String = ProjectSettings.globalize_path(chuck_file.resource_path)
+	var chuck_path: String = ProjectSettings.globalize_path(chuck_files[0].resource_path)
 	add_shred(chuck_path)
 	await get_tree().create_timer(2).timeout
 	var ids = get_shred_ids()
